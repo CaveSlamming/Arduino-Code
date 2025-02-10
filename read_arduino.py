f5944948af4618819793b7ec471ea66f7a661e22
ToisonedPurtle <poisonedturtles@gmail.com>
2025-02-10 15:43:09 -0500

    Arduino reading fixed

--- read_arduino.py
+++ read_arduino.py
@@ -4,5 +4,5 @@ import time
 arduino = serial.Serial(port='COM5', baudrate=9600, timeout=.1)
 
 while(True):
-    if(arduino.in_waiting):
+    if(arduino.in_waiting > 0):
         print(arduino.readLine())
